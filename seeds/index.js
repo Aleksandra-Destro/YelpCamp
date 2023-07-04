ce2efaac5d03f16a5987702a4857bfba2e8e2ba3
Aleksandra-Destro <121759568+Aleksandra-Destro@users.noreply.github.com>
2023-07-04 12:02:40 +0200

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+const axios = require("axios");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// unsplash collections.
+const collectionOne = "483251"; // woods collection
+const collectionTwo = "3846912"; //campgrounds collection
+const collectionThree = "9046579"; //camping
+
+async function seedImg(collection) {
+  try {
+    const resp = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: "PexKkJAQvLHffmBY17m5Afv03Fx0lh7P8jBpIGKDEgU",
+        collections: collection,
+        count: 30,
+      },
+      headers: { Accept: "application/json", "Accept-Encoding": "identity" },
+    });
+    return resp.data.map((a) => a.urls.small);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+
+  //make 3 API requests to unsplash, 30 images per request
+  const imageSetOne = await seedImg(collectionOne);
+  const imageSetTwo = await seedImg(collectionTwo);
+  const imageSetThree = await seedImg(collectionThree);
+
+  //spread into one array
+  const imgs = [...imageSetOne, ...imageSetTwo, ...imageSetThree]; // 90 random images
+
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const imgsSeed = Math.floor(Math.random() * imgs.length);
+    const camp = new Campground({
+      image: `${imgs[imgsSeed]}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, tenetur. Ea, inventore ducimus quaerat nemo excepturi sit architecto? Quis cum officiis alias placeat ad esse consequuntur mollitia dolores facilis voluptatem?",
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
