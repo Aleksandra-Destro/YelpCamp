a17726e3dee952864e4acdab843485df6544c87c
Aleksandra-Destro <aleksandra.destro@hotmail.com>
2023-07-26 13:49:43 +0200

    commit from 26.07.23

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/reviews");
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -49,7 +53,18 @@ app.use(
   })
 );
 
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret: "thisshouldbeabettersecret",
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR!", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
   secret: "thisshouldbeabettersecret",
   resave: false,
